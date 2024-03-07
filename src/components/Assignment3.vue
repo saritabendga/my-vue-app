7a45f0ca9ca2bf2656f34602263cf981fcd91201
saritabendga <79822444+saritabendga@users.noreply.github.com>
2024-03-07 09:30:20 +0530

    Add files via upload

---
+++ src/components/Assignment3.vue
@@ -0,0 +1,39 @@
+<template>
+	  <div>
+			<p  v-if="showItems">Conditionally Rendered Item </p>
+			<p v-for="(item, index) in items" :key="index">{{ item }} </p>
+		</div>
+	
+
+   <h2>Toggle Component</h2>
+    <button @click="toggleComponent">Toggle Component</button>
+    
+    <!-- Conditional rendering based on toggle state -->
+    <div v-if="toggleState">
+      <p>A</p>
+    </div>
+    <div v-else>
+      <p> B</p>
+    </div>
+  </div>
+  </template>
+
+	<script>
+	export default {
+		name:"App",
+		data() {
+			return {
+				showItems: true,
+        items: ['A', 'B', 'C'],
+        toggleState: true
+			},
+      methods: {
+        toggleComponent() {
+          this.toggleState = !this.toggleState;
+        }
+      }
+			}
+	}
+	</script>
+
+   
