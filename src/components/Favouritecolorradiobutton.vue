b6208dfc22d951617be40b2a1eb987df58462566
saritabendga <Sarita.Bendga@ibm.com>
2024-03-12 12:49:59 +0530

    Day7
    Day7

---
+++ src/components/Favouritecolorradiobutton.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>Choose your favorite color:</h2>
+    <label>
+      <input type="radio" v-model="selectedColor" value="red">
+      Red
+    </label>
+    <label>
+      <input type="radio" v-model="selectedColor" value="blue">
+      Blue
+    </label>
+    <label>
+      <input type="radio" v-model="selectedColor" value="green">
+      Green
+    </label>
+    <p>Your favorite color is: {{ selectedColor }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedColor: '' // Initialize selectedColor to store the selected value
+    }
+  }
+}
+</script>
